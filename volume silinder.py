0391fcb5e5dfde77699c9baa1e95212945f97ad4
Ricoo1206 <ricosetiawan724@gmail.com>
2024-10-21 18:24:44 +0800

    Add files via upload

---
+++ volume silinder.py
@@ -0,0 +1,10 @@
+# program untuk menghitung volume silinder 
+radius = 5   # mendefinisikan variabel radius dan memberikan nilai 5
+tinggi = 10  # mendefinisikan variabel tinggi dan memberikan nilai 10
+phi = 3.14   # mendefinisikan variabel phi dan memberikan nilai 2.14
+
+# menghitung volume silinder dengan rumus pi * r^2 * tinggi
+volume = phi * radius**2 * tinggi
+
+# menampilkan hasil volume silinder
+print("volume silinder:", volume)
